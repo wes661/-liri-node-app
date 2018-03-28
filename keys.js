a2264c58731271c2023aed4563ca5b0e9ec8db69
Wesley Hanson <wrh1600@gmail.com>
2018-03-27 23:10:22 -0700

    all commands functional

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 
 
-console.log('this is loaded');
+
 
 
 
--- liri.js
+++ liri.js
@@ -1,37 +1,54 @@
 require("dotenv").config();
+var fs = require("fs");
 var request = require("request");
-var Twitter = require('twitter');
-var Spotify = require('node-spotify-api');
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 var keys = require(__dirname + "/keys.js"); 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var omdbApi = (keys.omdb);
 var command = process.argv[2];
 var title = process.argv[3];
-var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=" + omdbApi.key;
-var queryUrl2 = "http://www.omdbapi.com/?t=mr%20nobody&y=&plot=short&apikey=" + omdbApi.key;
 
 
 
-if(command === "movie-this" && !title){
-    request(queryUrl2, function(error, response, body){
-        if (!error && response.statusCode === 200){
-            var body = JSON.parse(body);
-            console.log("Title: " + body.Title);
-            console.log("Year: " + body.Year);
-            console.log("IMDB Rating: " + body.imdbRating);
-            console.log("Rotten Tomatoes Score: " + body.Ratings[1].Value);
-            console.log("Country produced in: " + body.Country);
-            console.log("Language: " + body.Language);
-            console.log("Plot: " + body.Plot);
-            console.log("Actors: " + body.Actors);
-        }
-    })
-}
-else if(command === "movie-this"){
+switch(command){
+    case "my-tweets":
+      myTweets();
+    break;
+  
+    case "spotify-this-song":
+      if(title){
+        spotifySong(title);
+      } else{
+        spotifySong('The Sign');
+      }
+    break;
+  
+    case "movie-this":
+      if(title){
+        movieThis(title)
+      } else{
+        movieThis("Mr. Nobody")
+      }
+    break;
+  
+    case "do-what-it-says":
+      doThing();
+    break;
+
+    default:
+    console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
+    break;
+  
+  }
+
+function movieThis(movie){
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=" + omdbApi.key;
     request(queryUrl, function(error, response, body){
         if (!error && response.statusCode === 200){
             var body = JSON.parse(body);
+            console.log("----------------------------------------------------------------");
             console.log("Title: " + body.Title);
             console.log("Year: " + body.Year);
             console.log("IMDB Rating: " + body.imdbRating);
@@ -40,44 +57,47 @@ else if(command === "movie-this"){
             console.log("Language: " + body.Language);
             console.log("Plot: " + body.Plot);
             console.log("Actors: " + body.Actors);
+            console.log("----------------------------------------------------------------");
         }
     })
 }
 
-if(command === "my-tweets"){
-    var params = {screen_name: 'WesHanson16'};
-    client.get('statuses/user_timeline', params, function(error, tweets, body, response) {
+function myTweets(){
+    var params = {screen_name: 'WesHanson16',
+                  count: 20};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
             for (i = 0; i < tweets.length; i++){
+                console.log("----------------------------------------------------------------");
                 console.log(tweets[i].text);
                 console.log(tweets[i].created_at);
-            }    
+                console.log("----------------------------------------------------------------");
+            } 
+              
         }
     });
 }
-if(command === "spotify-this-song" && !title){
-    spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
+
+function spotifySong(song){
+    spotify.search({ type: 'track', query: song }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
         else{ var songInfo = data.tracks.items[0];
+            console.log("----------------------------------------------------------------");
             console.log(songInfo.artists[0].name)
             console.log(songInfo.name)
             console.log(songInfo.album.name)
             console.log(songInfo.preview_url)
+            console.log("----------------------------------------------------------------");
         }    
     });
 }
-else if(command === "spotify-this-song"){
-    spotify.search({ type: 'track', query: title }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-        else{ var songInfo = data.tracks.items[0];
-            console.log(songInfo.artists[0].name)
-            console.log(songInfo.name)
-            console.log(songInfo.album.name)
-            console.log(songInfo.preview_url)
-        }    
+
+function doThing(){
+    fs.readFile('random.txt', "utf8", function(error, data){
+      var txt = data.split(',');
+  
+      spotifySong(txt[1]);
     });
 }
