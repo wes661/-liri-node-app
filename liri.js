55c9903151d4591a6a83a078198e66770f4bb82f
Wesley Hanson <wrh1600@gmail.com>
2018-03-28 20:50:43 -0700

    adding homework bonus and finishing touches

--- liri.js
+++ liri.js
@@ -48,7 +48,6 @@ function movieThis(movie){
     request(queryUrl, function(error, response, body){
         if (!error && response.statusCode === 200){
             var body = JSON.parse(body);
-            console.log("----------------------------------------------------------------");
             console.log("Title: " + body.Title);
             console.log("Year: " + body.Year);
             console.log("IMDB Rating: " + body.imdbRating);
@@ -57,9 +56,24 @@ function movieThis(movie){
             console.log("Language: " + body.Language);
             console.log("Plot: " + body.Plot);
             console.log("Actors: " + body.Actors);
+            fs.appendFile("log.txt", "Title: " + body.Title + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "Year: " + body.Year + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "IMDB Rating: " + body.imdbRating + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "Rotten Tomatoes Score: " + body.Ratings[1].Value + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "Country produced in: " + body.Country + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "Language: " + body.Language + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "Plot: " + body.Plot + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "Actors: " + body.Actors + "\r\n", (error) => { /* handle error */ });
+            if(movie === "Mr. Nobody"){
+                console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+                console.log("It's on Netflix!");
+                fs.appendFile('log.txt', "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/" + "\r\n", (error) => { /* handle error */ });
+                fs.appendFile('log.txt', "It's on Netflix!" + "\r\n", (error) => { /* handle error */ });
+            }
             console.log("----------------------------------------------------------------");
+            fs.appendFile("log.txt", "----------------------------------------------------------------" + "\r\n", (error) => { /* handle error */ });
         }
-    })
+    });
 }
 
 function myTweets(){
@@ -68,10 +82,12 @@ function myTweets(){
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
             for (i = 0; i < tweets.length; i++){
+                console.log("Tweet: " + tweets[i].text);
+                console.log("Date " + tweets[i].created_at);
                 console.log("----------------------------------------------------------------");
-                console.log(tweets[i].text);
-                console.log(tweets[i].created_at);
-                console.log("----------------------------------------------------------------");
+                fs.appendFile("log.txt", "Tweet: " + tweets[i].text + "\r\n", (error) => { /* handle error */ });
+                fs.appendFile("log.txt", "Date " + tweets[i].created_at + "\r\n", (error) => { /* handle error */ });
+                fs.appendFile("log.txt", "----------------------------------------------------------------" + "\r\n", (error) => { /* handle error */ });
             } 
               
         }
@@ -84,12 +100,16 @@ function spotifySong(song){
           return console.log('Error occurred: ' + err);
         }
         else{ var songInfo = data.tracks.items[0];
+            console.log("Artist: " + songInfo.artists[0].name)
+            console.log("Track: " + songInfo.name)
+            console.log("Album: " + songInfo.album.name)
+            console.log("Preview " + songInfo.preview_url)
             console.log("----------------------------------------------------------------");
-            console.log(songInfo.artists[0].name)
-            console.log(songInfo.name)
-            console.log(songInfo.album.name)
-            console.log(songInfo.preview_url)
-            console.log("----------------------------------------------------------------");
+            fs.appendFile("log.txt", songInfo.artists[0].name + "\r\n", (error) => { /* handle error */ })
+            fs.appendFile("log.txt", songInfo.name + "\r\n", (error) => { /* handle error */ })
+            fs.appendFile("log.txt", songInfo.album.name + "\r\n", (error) => { /* handle error */ })
+            fs.appendFile("log.txt", songInfo.preview_url + "\r\n", (error) => { /* handle error */ })
+            fs.appendFile("log.txt", "----------------------------------------------------------------" + "\r\n", (error) => { /* handle error */ });
         }    
     });
 }
